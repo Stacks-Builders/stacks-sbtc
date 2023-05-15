9e8645a553b5d5640e56733c38c73e444db7fc72
José <30682875+jo-tm@users.noreply.github.com>
2023-05-15 11:29:24 -0700

    include clariform spread formatting

--- sbtc-mini/scripts/test.sh
+++ sbtc-mini/scripts/test.sh
@@ -2,4 +2,12 @@
 mkdir -p .test
 mkdir -p .coverage
 clarinet run --allow-write ext/generate-tests.ts
+rm -fR contracts-backup
+cp -R contracts contracts-backup
+rm -fR contracts
+docker run -v `pwd`:/home ghcr.io/prompteco/clariform --format=spread --output-dir "contracts" contracts-backup/*.clar
+mkdir contracts
+cp -R contracts-spread/contracts-backup/* contracts
 clarinet test --coverage .coverage/lcov.info .test
+rm -fR contracts
+cp -R contracts-backup contracts
