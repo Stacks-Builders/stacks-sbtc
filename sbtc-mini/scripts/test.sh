1fab1e0d807ee3fd17ef66118451a761f10876c8
José <30682875+jo-tm@users.noreply.github.com>
2023-05-23 06:51:28 -0700

    `pwd`->$(pwd)

--- sbtc-mini/scripts/test.sh
+++ sbtc-mini/scripts/test.sh
@@ -5,7 +5,7 @@ clarinet run --allow-write ext/generate-tests.ts
 mkdir -p contracts-backup
 cp -R contracts/*.clar contracts-backup/
 rm -fR contracts/*.clar
-docker run -v `pwd`:/home ghcr.io/prompteco/clariform --format=spread --output-dir "contracts-spread" contracts-backup/*.clar
+docker run -v $(pwd):/home ghcr.io/prompteco/clariform --format=spread --output-dir "contracts-spread" contracts-backup/*.clar
 cp -R contracts-spread/contracts-backup/* contracts
 clarinet test --coverage .coverage/lcov.info .test
 sudo rm -fR contracts/*.clar
